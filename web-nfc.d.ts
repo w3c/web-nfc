0c82a92de484f6d467922123162638f55cd760bd
Drew Williams <drewmwilliams55@gmail.com>
2021-11-19 16:26:46 -0800

    Added declare keyword; removed string type from NDEFReaderTypes

--- web-nfc.d.ts
+++ web-nfc.d.ts
@@ -22,7 +22,7 @@ declare type NDEFRecordDataSource = string | BufferSource | NDEFMessageInit
 interface Window {
   NDEFRecord: NDEFRecord
 }
-type NDEFRecordTypes = string | "absolute-url" | "empty" | "mime" | "smart-poster" | "text" | "unknown" | "url";
+declare type NDEFRecordTypes = "absolute-url" | "empty" | "mime" | "smart-poster" | "text" | "unknown" | "url";
 
 declare class NDEFRecord {
   constructor(recordInit: NDEFRecordInit)
